b2375c9ea2c8e4d56d2f6c4276205e453d0b42d1
Javid Magsudov <97388861+MJakoo@users.noreply.github.com>
2023-01-01 15:42:46 +0400

    Added additional page for Movies

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import Home from "./Home";
 import Top100 from "./Top100";
 import AboutUs from './AboutUs';
 import NotFound from './NotFound';
+import PageMovie from './pageMovie';
 
 function App() {
   return (
   <>
   <NavBar />
   <Routes>
+        
         <Route path="/" element=<Home /> />
+        <Route path="/top100/:pagemovie" element=<PageMovie /> />
         <Route path="/top100" element=<Top100 /> />
         <Route path="*" element=<NotFound/> />
         <Route path="/aboutus" element=<AboutUs /> />
---
+++ src/pageMovie.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './pageMovie.css';
+// import { useParams } from "react-router";
+// import data from "./recources/imdb_top_1000.json"
+
+export default function PageMovie() {
+    return (
+            <div className="Movie">
+                <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg" alt="Movie Poster" />
+                <p className="movieTitle" to="/top100/1">1. The Shawshank Redemption (1994)</p>
+                <p className="rating">IMDb: 9.3  Meta Score: 80%</p>
+              </div>
+      );
+}
