4a50109e93362fadd32e9e712ebb15e4db23fba8
FarazBaghernezhad <fbaghernezhad11788@ada.edu.az>
2023-01-03 00:29:06 +0400

    m

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import  React from 'react';
+import "./Home.css";
 
 export default function Home() {
     return <div>Where to look movie's rank? A movie evening always starts with a very time-consuming and frustrating search for which movies to watch. IMDbADA is there to help you find all 
