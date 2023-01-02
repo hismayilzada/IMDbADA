ce0121058183201f0a9094ac722f9545884c3971
Javid Magsudov <97388861+MJakoo@users.noreply.github.com>
2023-01-02 19:47:09 +0400

    Working List of TOP 1000

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -2,32 +2,37 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Product from './Product';
+import axios from "axios";
 import "./Movie.css";
 
 export default function Movie(props) {
 
   const [movieCount, setMovieCount] = useState(1);
-  const [movie, setMovie] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [id, setId] = useState();
+
+  // useEffect(() => {
+  //   let url = `http://localhost:2000/movies/`;
+  //   fetch(url)
+  //     .then((data) => {
+  //       return data.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setMovie(data);
+  //     });
+  // }, []);
 
   useEffect(() => {
-    let url = `http://localhost:2000/movies/${movieCount}`;
-    fetch(url)
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMovie(data);
-      });
-  }, [movieCount]);
-
-    console.log(movie);
+    axios.get("http://localhost:2000/movies/").then(async(res) => setMovies(res.data))
+  },[])
 
     return (
-      <div className="List">
-          <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg" alt="" />
-          <NavLink className="movieTitle" to="/movie/1">1. The Shawshank Redemption (1994)</NavLink>
-        </div>
+      <div>
+      {movies.map((movie) => (
+        <Product id={movie.id} img={movie.Poster_Link} title={movie.Series_Title} year={movie.Released_Year}/>
+      ))}
+      </div>
     );
   }
   
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import PageMovie from "./PageMovie";
+import { NavLink } from "react-router-dom";
 
 export default function Product(props) {
+  console.log(props)
   return (
-    <div className="movie-info">
-      <h3 className="movie-info--title">{props.Series_Title}</h3>
-      <p className="movie-info--year">Release Date: ${props.Released_Year}</p>
-            <img src={props.Poster_Link} alt="poster" />
-    </div>
+    <div className="List" id={props.id}>
+          <div className="poster"><img src={props.img} alt="" /></div>
+          <div className="movieInf">
+            <NavLink className="movieTitle" to={`/movie/${props.id}`}>{props.id}. {props.title} ({props.year})</NavLink>
+            <p className="inf">Ratings: IMDb: {props.IMDB_Rating}, MetaScore: {props.Meta_score}</p>
+            <PageMovie id={props.id}/>
+          </div>
+        </div>
   );
 }
