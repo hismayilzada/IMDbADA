e022172371617e49a4d53171ee1f65e1464f5fec
FarazBaghernezhad <fbaghernezhad11788@ada.edu.az>
2023-01-03 00:10:51 +0400

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import  React from 'react';
 
 export default function Home() {
-    return <div>This is the Home.</div>;
+    return <div>Where to look movie's rank? A movie evening always starts with a very time-consuming and frustrating search for which movies to watch. IMDbADA is there to help you find all 
+      the best-ranking movies you can stream legally and make your life easier.</div>;
   }
   
